837813cb2afcfb22a6ad2a5e3a984d7126bb002f
Antoine Gaillard <antoine.gaillard@datadoghq.com>
2025-11-28 12:10:13 +0100

    Revert "Add PV migration progress tracking with goroutine deduplication"
    This reverts commit f6db204f5af0b059fea7d776255ff870d133bd7c.

--- pkg/azuredisk/azuredisk_option.go
+++ pkg/azuredisk/azuredisk_option.go
@@ -73,7 +73,6 @@ type DriverOptions struct {
 	ConcurrentFormatTimeout            int64
 	GoMaxProcs                         int64
 	EnableMigrationMonitor             bool
-	EnablePVMigrationTracking          bool
 }
 
 func (o *DriverOptions) AddFlags() *flag.FlagSet {
@@ -128,6 +127,5 @@ func (o *DriverOptions) AddFlags() *flag.FlagSet {
 	fs.Int64Var(&o.ConcurrentFormatTimeout, "concurrent-format-timeout", 300, "maximum time in seconds duration of a format operation before its concurrency token is released")
 	fs.Int64Var(&o.GoMaxProcs, "max-procs", 2, "maximum number of CPUs that can be executing simultaneously in golang runtime")
 	fs.BoolVar(&o.EnableMigrationMonitor, "enable-migration-monitor", true, "enable migration monitor for Azure Disk CSI Driver")
-	fs.BoolVar(&o.EnablePVMigrationTracking, "enable-pv-migration-tracking", false, "enable PV migration progress tracking")
 	return fs
 }
